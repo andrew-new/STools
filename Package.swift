9399a3ccc8bc79c39b7e6021e0206a13aa2c9ca2
behring <behring.zhao@gmail.com>
2023-08-11 21:12:03 +0800

    Update Package.swift
    Modify macOS(.v10_15) to macOS(.v10_13)

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "STools",
     platforms: [
            .iOS(.v13),
-           .macOS(.v10_15),
+           .macOS(.v10_13),
            .watchOS(.v6)
        ],
     products: [
